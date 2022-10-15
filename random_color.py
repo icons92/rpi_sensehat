2ae14efee661690382c50a624a27ff2cc8ed80d9
Isaac Considine <icons92@gmail.com>
2022-10-15 09:16:24 -0600

    loop added

--- random_color.py
+++ random_color.py
@@ -8,15 +8,16 @@ s = SenseHat()
 s.set_rotation(180)
 
 try:
-    logo = []
-    for i in range(0,63):
-        print(i)
-        logo.append(list(np.random.choice(range(256), size=3)))
+    while True:
+        logo = []
+        for i in range(0,63):
+            print(i)
+            logo.append(list(np.random.choice(range(256), size=3)))
 
-    s.set_pixel(logo)
-    time.sleep(1)
-    logo.clear()
-
+        s.set_pixel(logo)
+        time.sleep(1)
+        logo.clear()
+    
 except KeyboardInterrupt:
    	
     s.clear()
