4bf298b4757cf624a8a6f52f1a4371103b8683c2
Isaac Considine <icons92@gmail.com>
2022-10-15 09:09:37 -0600

    Code in random color

---
+++ random_color.py
@@ -0,0 +1,23 @@
+from curses import KEY_A1
+import numpy as np
+from sense_hat import SenseHat
+
+import time
+
+s = SenseHat()
+s.set_rotation(180)
+
+try:
+    logo = []
+    for i in range(0,63):
+        print(i)
+        logo.append(list(np.random.choice(range(256), size=3)))
+
+    s.set_pixel(logo)
+    time.sleep(1)
+    logo.clear()
+
+except KeyboardInterrupt:
+   	
+    s.clear()
+    pass
