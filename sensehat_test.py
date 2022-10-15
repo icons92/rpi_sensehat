92341244aefd12f9606bd84953025d414e2ffeeb
Isaac Considine <icons92@gmail.com>
2022-10-14 18:06:22 -0600

    Basic Sense Hat Project

---
+++ sensehat_test.py
@@ -0,0 +1,67 @@
+from sense_hat import SenseHat
+import time
+
+s = SenseHat()
+s.low_light = True
+
+green = (0, 255, 0)
+red = (255, 0, 0)
+white = (255,255,255)
+nothing = (0,0,0)
+pink = (255,105, 180)
+
+def raspi_logo():
+    G = green
+    R = red
+    O = nothing
+    logo = [
+    O, G, G, O, O, G, G, O, 
+    O, O, G, G, G, G, O, O,
+    O, O, R, R, R, R, O, O, 
+    O, R, R, R, R, R, R, O,
+    R, R, R, R, R, R, R, R,
+    R, R, R, R, R, R, R, R,
+    O, R, R, R, R, R, R, O,
+    O, O, R, R, R, R, O, O,
+    ]
+    return logo
+
+def equals():
+    W = white
+    O = nothing
+    logo = [
+    O, O, O, O, O, O, O, O, 
+    O, W, W, W, W, W, W, O,
+    O, W, W, W, W, W, W, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, W, W, W, W, W, W, O,
+    O, W, W, W, W, W, W, O,
+    O, O, O, O, O, O, O, O,
+    ]
+    return logo
+
+def heart():
+    P = pink
+    O = nothing
+    logo = [
+    O, O, O, O, O, O, O, O,
+    O, P, P, O, P, P, O, O,
+    P, P, P, P, P, P, P, O,
+    P, P, P, P, P, P, P, O,
+    O, P, P, P, P, P, O, O,
+    O, O, P, P, P, O, O, O,
+    O, O, O, P, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    ]
+    return logo
+
+images = [raspi_logo, equals, heart]
+count = 0
+
+while True:
+    s.set_pixels(images[count % len(images)]())
+    time.sleep(.75)
+    count += 1
+    if count > 10:
+        break
