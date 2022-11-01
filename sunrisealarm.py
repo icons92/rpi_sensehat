677f3c8b2c2db4743c21edea8c272bfdd3e9413a
Isaac Considine <icons92@gmail.com>
2022-11-01 12:00:20 -0600

    clock display when triggered

--- sunrisealarm.py
+++ sunrisealarm.py
@@ -7,28 +7,31 @@ import time
 s = SenseHat()
 s.set_rotation(180)
 
-alarm = datetime.strptime("11:45AM", '%I:%M%p').strftime('%I:%M%p')
+alarm = datetime.strptime("12:05PM", '%I:%M%p').strftime('%I:%M%p')
+a_trigger = False
 
 try:
     
     sunrise = []
     for i in range(0,64):
            
-        sunrise.insert(0,list([247,205,93]))
+        sunrise.insert(0,list([255,205,93]))
 
     while True: 
 
         time.sleep(10)       
         now = datetime.now().strftime('%I:%M%p')
 
-        if now == alarm:         
+        if now == alarm:
+            a_trigger = True         
             s.set_pixels(sunrise)
-        elif now > alarm:
-            print('now is greater')
-            print('{} > {}'.format(now, alarm))
-        elif now < alarm:
-            print('now is less than alarm')
-            print('{} < {}'.format(now, alarm))
+        elif now > alarm:            
+            pass
+        elif now < alarm:            
+            pass
+
+        if a_trigger:
+            s.show_message(now, text_colour=(0,0,0))
         
         
     
