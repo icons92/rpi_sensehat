2f95a0aeee8f5a11b6329b7ca12a0f8bd0b4396c
icons92 <icons92@gmail.com>
2022-11-01 12:18:27 -0600

    indentation error

--- sunrisealarm.py
+++ sunrisealarm.py
@@ -7,19 +7,21 @@ import time
 s = SenseHat()
 s.set_rotation(180)
 
-alarm = datetime.strptime("12:05PM", '%I:%M%p').strftime('%I:%M%p')
+alarm = datetime.strptime("12:13PM", '%I:%M%p').strftime('%I:%M%p')
 a_trigger = False
+black = [0,0,0]
 
 try:
     
     sunrise = []
     for i in range(0,64):
            
-        sunrise.insert(0,list([255,205,93]))
+        sunrise.append([255,205,93])
 
-    while True: 
-
-        time.sleep(10)       
+    while True:
+	
+	print(sunrise)
+	time.sleep(10)
         now = datetime.now().strftime('%I:%M%p')
 
         if now == alarm:
@@ -31,7 +33,7 @@ try:
             pass
 
         if a_trigger:
-            s.show_message(now, text_colour=(0,0,0))
+            s.show_message(now, text_colour= black, back_colour= sunrise)
         
         
     
