49b602b670336120ce740bf307e0bd0b2da935d6
Isaac Considine <icons92@gmail.com>
2022-11-01 11:36:38 -0600

    sunrise

--- sunrisealarm.py
+++ sunrisealarm.py
@@ -1,11 +1,13 @@
 import numpy as np
 from sense_hat import SenseHat
-import datetime
+from datetime import datetime
 
 import time
 
 s = SenseHat()
 s.set_rotation(180)
+
+ alarm = datetime.strptime("11:40AM", '%I:%M%p').strftime('%I:%M%p')
 
 try:
     
@@ -14,9 +16,19 @@ try:
            
         sunrise.insert(0,list([247,205,93]))
 
-              
-    s.set_pixels(sunrise)
-    
+    while True: 
+
+        time.sleep(.5)       
+        now = datetime.now().strftime('%I:%M%p')
+
+        if now == alarm:         
+            s.set_pixels(sunrise)
+        elif now > alarm:
+            print('now is greater')
+            print(f'{now} > {alarm}')
+        elif now < alarm:
+            print('now is less than alarm')
+            print(f'{now} < {alarm}')
         
         
     
